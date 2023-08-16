b932648150017e7e6e4d366ad54021d7ff838d00
QEDELouw <Etienne.Louw@qedact.com>
2023-08-16 11:05:28 +0200

    Day 1 Exercise 3 Completed.

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,5 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print('Euclidian distance between (2, 3) and (10, 8): ' + str(((2 - 10) ** 2 + (3 - 8) ** 2) ** 0.5))
