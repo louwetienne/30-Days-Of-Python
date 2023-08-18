16e3689a686e2f5b8056117a5785635107ededca
QEDELouw <Etienne.Louw@qedact.com>
2023-08-18 09:26:38 +0200

    Day 2 Completed

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -38,3 +38,32 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+print('Length first name: ' + str(len(first_name)))
+print('Length last name: ' + str(len(last_name)))
+print('Length Difference between first and last name: ' + str(len(first_name) - len(last_name)))
+
+
+numOne = 5
+numTwo = 4
+total = numOne + numTwo
+diff = numOne - numTwo
+division = numOne / numTwo
+modulas = numOne % numTwo
+exp = numOne ** numTwo
+floor_division = numOne // numTwo
+
+
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+
+radius = input('What is the radius of the cirlce: ')
+radius = float(radius)
+
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+
+print('Area of the circle: ', area_of_circle)
+print('Circumference of the circle: ', circum_of_circle)
+
